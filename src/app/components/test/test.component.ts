bf3d083553985502ece5f2d7f77d8475c4e4678c
vojajokic <55879745+vojajokic@users.noreply.github.com>
2024-09-10 13:54:45 +0000

    Izmene u podešavanjima Codespace i component

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-test',
+    template: `
+        <h1>Welcome to Test component </h1>
+        <p>This is a test component that was created manually</p>
+    `,
+    styles: ['h1 {color: red}']
+})
+
+export class TestComponent {
+
+}
