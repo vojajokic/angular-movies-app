eac40e0e8ab9117d3288a09edd89fb37cf991d83
vojajokic <55879745+vojajokic@users.noreply.github.com>
2024-09-11 13:47:09 +0000

    Test Interpolation

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class DetailsComponent implements OnInit {
+export class DetailsComponent implements OnInit, OnChanges, AfterContentInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('ngOnInit called')
+  }
+  ngOnChanges(): void {
+    console.log('ngOnChanges called')
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit called')
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit called')
   }
 
 }
