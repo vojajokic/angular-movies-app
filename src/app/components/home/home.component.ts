0a93a4b732d7e661ef3d2203e5acd7674f379b41
vojajokic <55879745+vojajokic@users.noreply.github.com>
2024-09-30 11:40:58 +0000

    *ngFor Structural Directive

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -13,6 +13,64 @@ export class HomeComponent implements OnInit {
   }
 
   isVisible = true;
+
+  fanFavoriteMovie: any[] = 
+  [
+    {
+      imageUrl: '../../../assets/images/card-image.png',
+      title: 'Movie #1 Title',
+      actors: 'Movie #1 actors',
+      year: '2023',
+      rate: 9.2,
+      rank: 1
+    },
+    {
+      imageUrl: '../../../assets/images/card-image.png',
+      title: 'Movie #2 Title',
+      actors: 'Movie #2 actors',
+      year: '2023',
+      rate: 9.5,
+      rank: 2
+    },
+    {
+      imageUrl: '../../../assets/images/card-image.png',
+      title: 'Movie #3 Title',
+      actors: 'Movie #3 actors',
+      year: '2023',
+      rate: 8.5,
+      rank: 5
+    },
+    {
+      imageUrl: '../../../assets/images/card-image.png',
+      title: 'Movie #4 Title',
+      actors: 'Movie #4 actors',
+      year: '2023',
+      rate: 9.5,
+      rank: 1
+    },
+    
+  ]
+  topMovies: any[] = 
+  [
+    {
+      imageUrl: '../../../assets/images/card-image.png',
+      title: 'Movie #1 Title',
+      actors: 'Movie #1 actors',
+      year: '2023',
+      rate: 9.2,
+      rank: 1
+    },
+    {
+      imageUrl: '../../../assets/images/card-image.png',
+      title: 'Movie #2 Title',
+      actors: 'Movie #2 actors',
+      year: '2023',
+      rate: 9.5,
+      rank: 2
+    },
+    
+  ]
+  
    // currentStyle: Record<string, string> = {};
   // isCentered = false;
   // isSuccessed = false;
