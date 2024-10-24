d151fb91c78bcd4133c7f08daa7242827b7f3f8c
vojajokic <55879745+vojajokic@users.noreply.github.com>
2024-10-24 11:08:03 +0000

    Validating Reactive Forms

--- src/app/components/feedback/feedback.component.ts
+++ src/app/components/feedback/feedback.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Feedback } from '../data/model/Feedback';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback',
@@ -12,16 +12,16 @@ export class FeedbackComponent implements OnInit {
   // feedback: Feedback = new Feedback('','',8,'','');
   feedbackForm!: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.feedbackForm = this.fb.group({
-      emailAddress: [''],
-      phoneNumber: [''],
-      rate: [8],
-      feedbackTitle: [''],
-      feedback: ['']
+    this.feedbackForm = new FormGroup({
+      emailAddress: new FormControl('', [Validators.required, Validators.email]),
+      phoneNumber: new FormControl('', [Validators.pattern('[0-9]{4}-[0-9]{4}')]),
+      rate: new FormControl(8, [Validators.min(0), Validators.max(10)]),
+      feedbackTitle: new FormControl('', [Validators.required]),
+      feedback: new FormControl('', [Validators.required])
     })
   }
 
