81841acfd1de421d2135fc6914984f255d01d252
vojajokic <55879745+vojajokic@users.noreply.github.com>
2024-10-31 14:39:46 +0000

    Create User Profile Section

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -9,11 +9,18 @@ import { AuthService } from '@auth0/auth0-angular';
 export class NavbarComponent implements OnInit {
 
   public searchMovieTitle = 'Proba';
+  public loggedInUser: any;
 
   constructor(public _auth: AuthService) { 
   }
 
   ngOnInit(): void {
+    if(this._auth.user$) {
+      this._auth.user$.subscribe((data) => {
+        this.loggedInUser = data;
+        console.log('LoggedIn User = ', this.loggedInUser);
+      })
+    }
   }
 
 }
